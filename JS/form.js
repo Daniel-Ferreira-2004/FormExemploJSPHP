ec7c8daf8f84c06e6d716f325d8f8c7ad1d278d9
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-05-22 17:48:17 -0300

    JS documentado

--- JS/form.js
+++ JS/form.js
@@ -1,13 +1,16 @@
-'use strict'
+'use strict' /*Usado usado um modo mais seguro e rigoroso para executar o código.*/
 
-const FormId = document.getElementById('FormID');
+const FormId = document.getElementById('FormID'); /*Declarei uma variavel para pegar o elemento FormID do HTML pelo ID*/
 
-const togglerContainer = () => FormID.classList.toggle('togglerContainer');
+const togglerContainer = () => FormID.classList.toggle('togglerContainer'); /*Declarei uma variavel que usa o metodo arrow(Função) que vai pegar a variavel FormID, e adicionar uma classe com o classList, o metodo toggle, verifica se a classe já está lá, se sim, remove, se não, adiciona*/
 
 document.getElementById('btnOverlayRegister').addEventListener('click', togglerContainer);
-
+/*Pega o elemento ID de um botão de registro, e adiciona um evento ao Click, e chama a variavel togglerContainer*/
 document.getElementById('btnOverlayLogin').addEventListener('click', togglerContainer);
+/*Pega o elemento ID de um botão de Login, e adiciona um evento ao Click, e chama a variavel togglerContainer*/
 
+/*Pra celular*/
 document.getElementById('btnOverlayRegisterMobile').addEventListener('click', togglerContainer);
-
+/*Pega o elemento ID de um botão de mobile de Login, e adiciona um evento ao Click, e chama a variavel togglerContainer*/
 document.getElementById('btnOverlayLoginMobile').addEventListener('click', togglerContainer);
+/*Pega o elemento ID de um botão de mobile de Cadastrar, e adiciona um evento ao Click, e chama a variavel togglerContainer*/
