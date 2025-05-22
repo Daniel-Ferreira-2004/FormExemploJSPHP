bef362c4dcfe8833ebc4d2654c26e990b4149f77
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-05-22 10:23:36 -0300

    Responsividade Comcluida

--- JS/form.js
+++ JS/form.js
@@ -7,3 +7,7 @@ const togglerContainer = () => FormID.classList.toggle('togglerContainer');
 document.getElementById('btnOverlayRegister').addEventListener('click', togglerContainer);
 
 document.getElementById('btnOverlayLogin').addEventListener('click', togglerContainer);
+
+document.getElementById('btnOverlayRegisterMobile').addEventListener('click', togglerContainer);
+
+document.getElementById('btnOverlayLoginMobile').addEventListener('click', togglerContainer);
